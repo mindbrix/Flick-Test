3252f2d06ada75ab150739be3b887ce94fca2518
Nigel Barber <nigel@mindbrix.co.uk>
2018-10-01 12:05:48 +0100

    Initial Commit

---
+++ Flickr Test/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  Flickr Test
+//
+//  Created by Nigel Barber on 01/10/2018.
+//  Copyright © 2018 Nigel Barber. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
